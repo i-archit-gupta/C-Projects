0bb8f75d7481628b10524e872ed22719d4d35895
i-archit-gupta <guptarchit2k17@gmail.com>
2021-09-17 13:35:36 +0530

    Add two numbers code

---
+++ add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a,b;
+    printf("Enter number a\n",a);
+    scanf("%d", &a);
+
+    printf("Enter numebr b\n",b);
+    scanf("%d", &b);
+
+    printf("The sum is %d\n",a+b);
+    return 0;
+}
