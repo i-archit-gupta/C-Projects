5134170abecabea4c863da043461c73f82a7afd1
i-archit-gupta <guptarchit2k17@gmail.com>
2021-09-20 00:14:52 +0530

    Added Multiply Program

---
+++ multiply.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    printf("Enter the number of which you want multiplication table:\n");
+    scanf("%d", &t);
+    printf("The multiplication table is as follows:\n");
+    printf("%dX1=%d\n",t,t*1);
+    printf("%dX2=%d\n",t,t*2);
+    printf("%dX3=%d\n",t,t*3);
+    printf("%dX4=%d\n",t,t*4);
+    printf("%dX5=%d\n",t,t*5);
+    printf("%dX6=%d\n",t,t*6);
+    printf("%dX7=%d\n",t,t*7);
+    printf("%dX8=%d\n",t,t*8);
+    printf("%dX9=%d\n",t,t*9);
+    printf("%dX10=%d\n",t,t*10);
+    
+    return 0;
+}
