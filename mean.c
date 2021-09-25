b03de1d3f961e57cdd855783796ac86d3d8e09e5
i-archit-gupta <guptarchit2k17@gmail.com>
2021-09-25 21:57:10 +0530

    Mean Program modified

---
+++ mean.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    // Write a program in C to display n terms of natural number and their sum
+    int i,s=0,n;
+    printf("Enter the number of terms:\n");
+    scanf("%d",&n);
+    printf("The first %d natural numbers are:\n",n);
+    for(i=1;i<=n;i+=1)
+    {
+        printf("%d\n",i);
+        s+=i;
+    }
+    printf("The sum of first %d natural numbers is:%d\n",n,s);
+    return 0;
+}
